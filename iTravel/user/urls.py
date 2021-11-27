68e37f98e55db248c5c1f46f943285163ed87796
Nibedita <nibeditabehera615@gmail.com>
2021-11-27 18:07:01 +0530

    final submission

--- iTravel/user/urls.py
+++ iTravel/user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('register',views.user_register),
-    path('login',views.user_login)
+    path('login',views.user_login),
+    path('logout',views.user_logout)
 ]
